74787856357228d7cb85a7ed77bc42cdd35ebfab
jasmineerina15 <jasmineerina15@gmail.com>
2024-07-04 20:49:14 +0700

    Controller: Initialize Register

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -1,13 +1,40 @@
 package authcontroller
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/jasmineerina/go-jwt-mux/models"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Login(w http.ResponseWriter, r *http.Request) {
 
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	// mengambil inputan json
+	var userInput models.User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&userInput); err != nil {
+		log.Fatal("Gagal mendecode json")
+	}
+	defer r.Body.Close()
 
+	// hash password menggunakan Bcrpyt
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	userInput.Password = string(hashPassword)
+
+	// insert ke data
+	if err := models.DB.Create(&userInput).Error; err != nil {
+		log.Fatal("Gagal menyimpan data")
+	}
+
+	response, _ := json.Marshal(map[string]string{"message": "success"})
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
 
 func Logout(w http.ResponseWriter, r *http.Request) {
