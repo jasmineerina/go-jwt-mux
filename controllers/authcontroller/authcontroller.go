c4a35196217d8a6f0e294afaa624527cce013c6c
jasmineerina15 <jasmineerina15@gmail.com>
2024-07-04 21:26:16 +0700

    Initialize:Login checking if all the data is valid

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -7,10 +7,40 @@ import (
 	"github.com/jasmineerina/go-jwt-mux/helper"
 	"github.com/jasmineerina/go-jwt-mux/models"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	// mengambil inputan json
+	var userInput models.User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&userInput); err != nil {
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusBadRequest, response)
+	}
+	defer r.Body.Close()
 
+	// ambil data user berdasarkan username
+	var user models.User
+	if err := models.DB.Where("username = ?", userInput.Username).First(&user).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			response := map[string]string{"message": "Username atau Password salah"}
+			helper.ResponseJSON(w, http.StatusUnauthorized, response)
+			return
+		default:
+			response := map[string]string{"message": err.Error()}
+			helper.ResponseJSON(w, http.StatusInternalServerError, response)
+			return
+		}
+	}
+
+	// cek apakah password valid
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userInput.Password)); err != nil {
+		response := map[string]string{"message": "Username atau Password salah"}
+		helper.ResponseJSON(w, http.StatusUnauthorized, response)
+		return
+	}
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	Id          int64  `gorm:"primaryKey" json:"id"`
-	NamaLengkap string `gorm:"varchar(300)" json:"nama_lengkap`
-	Username    string `gorm:"varchar(300)" json:"username"`
-	Password    string `gorm:"varchar(300)" json:"password"`
+	NamaLengkap string `gorm:"varchar(300);not null" json:"nama_lengkap`
+	Username    string `gorm:"varchar(300);not null" json:"username"`
+	Password    string `gorm:"varchar(300);not null" json:"password"`
 }
