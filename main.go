25a532f970d8ffea24a4bdec8a587e34e38cd6e5
jasmineerina15 <jasmineerina15@gmail.com>
2024-07-04 20:40:37 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jasmineerina/go-jwt-mux/controllers/authcontroller"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
+	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
+	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
